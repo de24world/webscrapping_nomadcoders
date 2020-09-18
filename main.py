6e780315c42d183c6163746346678ca52ff2d061
Pusha <de24world@gmail.com>
2020-09-18 22:56:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+import requests
+
+indeed_result = requests.get("https://de.indeed.com/Jobs?as_and=tensorflow&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&st=&radius=25&l=germany&fromage=any&limit=50&sort=&psf=advsrch&from=advancedsearch")
+
